00c4cd25dbb97815988fd6445fd779001dc24cad
rhin0cer0s <christophe.guieu@gmail.com>
2022-04-24 13:13:42 +0200

    [EDIT] Color::from_hex() real bitwise operator | [ADD] ColorJsonEncoder

--- hex2mtn/Color.py
+++ hex2mtn/Color.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
+import json
 
 class Color() :
-	
+
 	def __init__(self, name : str, red : int = 0, green : int = 0, blue : int = 0) :
 		self.name	= name
 		self.red	= red
@@ -28,17 +29,21 @@ class Color() :
 		if colorInt > 0xFFFFFF or colorInt < 0 :
 			raise ValueError('{} is not a right HEX value.'.format(hexString))
 
-		self.red	= 0xFF0000 and colorInt >> 0x16
-		self.green	= 0x00FF00 and colorInt >> 0x8
-		self.blue	= 0x0000FF and colorInt
+		self.red	= (0xFF0000 & colorInt) >> 16
+		self.green	= (0x00FF00 & colorInt) >> 8
+		self.blue	= (0x0000FF & colorInt)
 
 		pass
 
 	def from_rgb(self, rgbString : str) -> None :
-		return
+		raise NotImplementedError
 
-	def distance(self, color : Color) -> tuple :
-		return
+	def distance(self, color : Color) -> int :
+		red		= (self.red - color.red)**2
+		green	= (self.green - color.green)**2
+		blue	= (self.blue - color.blue)**2
+
+		return red + green + blue
 
 	def hex(self) -> str :
 		hexOutput = "#{:02X}{:02X}{:02X}".format(self.red, self.green, self.blue) 
@@ -47,3 +52,11 @@ class Color() :
 	def rgb(self) -> str :
 		rgbOutput = "rgb({:03}, {:03}, {:03})".format(self.red, self.green, self.blue)
 		return rgbOutput
+
+	def reprJSON(self) -> dict :
+		return self.__dict__
+
+class ColorJsonEncoder(json.JSONEncoder) :
+	
+	def default(self, o: Color) -> dict:
+		return o.__dict__
